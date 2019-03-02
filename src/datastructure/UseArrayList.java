a425d962ed2dc797cb476e8bad25b201b90fd2c1
Manzoor Kamran <ManzoorKamran@Manzoors-MacBook-Pro.local>
2019-03-02 16:23:07 -0500

    First Commit

--- src/datastructure/UseArrayList.java
+++ src/datastructure/UseArrayList.java
@@ -6,7 +6,7 @@ public class UseArrayList {
 		/*
 		 * Demonstrate how to use ArrayList that includes add,peek,remove,retrieve elements.
 		 * Use For Each loop and while loop with Iterator to retrieve data.
-		 * Store all the sorted data into one of the databases.
+		 * Store all the sorted data into one of the databases.-no need to do this
 		 * 
 		 */
 	
