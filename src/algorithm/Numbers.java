f92d9dcc509094cefb6c8f5846e94641a86481bc
Manzoor Kamran <ManzoorKamran@Manzoors-MacBook-Pro.local>
2019-03-03 14:39:34 -0500

    Find missing number completion

--- src/algorithm/Numbers.java
+++ src/algorithm/Numbers.java
@@ -10,72 +10,114 @@ import java.util.Random;
  */
 public class Numbers {
 
-	/*
-	 * Show all the different kind of sorting algorithm by applying into (num array).
-	 * Display the execution time for each sorting.Example in below.
-	 *
-	 * Use any databases[MongoDB, Oracle or MySql] to store data and retrieve data.
-	 *
-	 * At the end. After running all the sorting algo, come to a conclusion which one is suitable on given data set.
-	 *
-	 */
+    /*
+     * Show all the different kind of sorting algorithm by applying into (num array).
+     * Display the execution time for each sorting.Example in below.
+     *
+     * Use any databases[MongoDB, Oracle or MySql] to store data and retrieve data.
+     *
+     * At the end. After running all the sorting algo, come to a conclusion which one is suitable on given data set.
+     *
+     */
 
-	public static void main(String[] args) throws Exception {
-		
-		int [] num = new int[10];
-		storeRandomNumbers(num);
-		ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
-		//Selection Sort
-		Sort algo = new Sort();
-		algo.selectionSort(num);
-		long selectionSortExecutionTime = algo.executionTime;
-		System.out.println("Total Execution Time of "+ num.length + " numbers in Selection Sort take: " + selectionSortExecutionTime + " milli sec");
-        connectToSqlDB.insertDataFromArrayToSqlTable(num, "selection_sort", "SortingNumbers");
-        List<String> numbers = connectToSqlDB.readDataBase("selection_sort", "SortingNumbers");
-        printValue(numbers);
-		int n = num.length;
-		randomize (num, n);
-		//Insertion Sort
-		algo.insertionSort(num);
-		long insertionSortExecutionTime = algo.executionTime;
-		System.out.println("Total Execution Time of " + num.length + " numbers in Insertion Sort take: " + insertionSortExecutionTime + " milli sec");
+    public static void main(String[] args) throws Exception {
 
-		//By following above, Continue for rest of the Sorting Algorithm....
+        int[] num = new int[100000];
+        storeRandomNumbers(num);
+
+        //Creating Sort class object
+        Sort algo = new Sort();
+
+        //Connection to DB
+        ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
+
+        //Selection Sort:
+        algo.selectionSort(num);
+        long selectionSortExecutionTime = algo.executionTime;
+        System.out.println("Total Execution Time of " + num.length + " numbers in Selection Sort take: " + selectionSortExecutionTime + " milli sec");
+        //Sort.printSortedArray(num);
+
+//        //Inserting the sorted data from the array to the SQL DB table
+//        connectToSqlDB.insertDataFromArrayToSqlTable(num, "selection_sort", "SortingNumbers");
+//        //Reading those sorted data from the DB and storing the value to the List
+//        List<String> numbers = connectToSqlDB.readDataBase("selection_sort", "SortingNumbers");
+//        //Printing those sorted data to the console
+//        printValue(numbers);
+
+        int n = num.length;
+        randomize(num, n);
+
+        //Insertion Sort:
+        algo.insertionSort(num);
+        long insertionSortExecutionTime = algo.executionTime;
+        System.out.println("Total Execution Time of " + num.length + " numbers in Insertion Sort take: " + insertionSortExecutionTime + " milli sec");
+        //Sort.printSortedArray(num);
+
+        //By following above, Continue for rest of the Sorting Algorithm....
+
+        //Bubble Sort:
+        randomize(num, n);
+        algo.bubbleSort(num);
+        long bubbleSortExecutionTime = algo.executionTime;
+        System.out.println("Total Execution Time of " + num.length + " numbers in Bubble Sort take: " + bubbleSortExecutionTime + " milli sec");
+        //Sort.printSortedArray(num);
+
+        //Merge Sort:
+        randomize(num, n);
+        algo.mergeSort(num, 0, n - 1);
+        long mergeSortExecutionTime = algo.executionTime;
+        System.out.println("Total Execution Time of " + num.length + " numbers in Merge Sort take: " + mergeSortExecutionTime + " milli sec");
+        //Sort.printSortedArray(num);
+
+        //Quick Sort:
+        randomize(num, n);
+        algo.quickSort(num, 0, n - 1);
+        long quickSortExecutionTime = algo.executionTime;
+        System.out.println("Total Execution Time of " + num.length + " numbers in Quick Sort take: " + quickSortExecutionTime + " milli sec");
+        //Sort.printSortedArray(num);
+
+        //Heap Sort:
+        randomize(num, n);
+        algo.heapSort(num);
+        int maxValueForBucketSort = num[num.length - 1];
+        long heapSortExecutionTime = algo.executionTime;
+        System.out.println("Total Execution Time of " + num.length + " numbers in Heap Sort take: " + heapSortExecutionTime + " milli sec");
+        //Sort.printSortedArray(num);
+
+        //Bucket Sort:
+        randomize(num, n);
+        algo.bucketSort(num, maxValueForBucketSort);
+        long bucketSortExecutionTime = algo.executionTime;
+        System.out.println("Total Execution Time of " + num.length + " numbers in Bucket Sort take: " + bucketSortExecutionTime + " milli sec");
+        //Sort.printSortedArray(num);
+    }
 
 
+    //Come to conclusion about which Sorting Algo is better in given data set.
 
 
+    public static void storeRandomNumbers(int[] num) {
+        Random rand = new Random();
+        for (int i = 0; i < num.length; i++) {
+            num[i] = rand.nextInt(100000);
+        }
+    }
 
+    public static void randomize(int arr[], int n) {
+        Random r = new Random();
+        // Start from the last element and swap one by one. We don't
+        // need to run for the first element that's why i > 0
+        for (int i = n - 1; i > 0; i--) {
+            int j = r.nextInt(i);
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
 
-
-
-
-		//Come to conclusion about which Sorting Algo is better in given data set.
-
-	}
-
-	public static void storeRandomNumbers(int [] num){
-		Random rand = new Random();
-		for(int i=0; i<num.length; i++){
-			num[i] = rand.nextInt(1000000);
-		}
-	}
-
-	public static void randomize( int arr[], int n)
-	{
-		Random r = new Random();
-		// Start from the last element and swap one by one. We don't
-		// need to run for the first element that's why i > 0
-		for (int i = n-1; i > 0; i--) {
-			int j = r.nextInt(i);
-			int temp = arr[i];
-			arr[i] = arr[j];
-			arr[j] = temp;
-		}
-	}
-	public static void printValue(List<String> array){
-		for(String st:array){
-			System.out.println(st);
-		}
-	}
+    public static void printValue(List<String> array) {
+        for (String st : array) {
+            System.out.println(st);
+        }
+    }
 }
--- src/algorithm/UnitTestSorting.java
+++ src/algorithm/UnitTestSorting.java
@@ -8,24 +8,101 @@ public class UnitTestSorting {
       This class is about Unit testing for Sorting Algorithm.
      */
     public static void main(String[] args) {
-        int [] unSortedArray = {6,9,2,5,1,0,4};
-        int [] sortedArray =   {0,1,2,4,5,6,9};
+        int[] unSortedArray = {6, 9, 2, 5, 1, 0, 4};
+        int[] sortedArray = {0, 1, 2, 4, 5, 6, 9};
+
         //Create Sort object
         Sort sort = new Sort();
-        //apply unsorted array to selectionSort.
+
+        //Apply unsorted array to selectionSort.
+        //Selection Sort:
         sort.selectionSort(unSortedArray);
-        //verify if the unsorted array is sorted by the selection sort algorithm.
+
+        //Verify if the unsorted array is sorted by the selection sort algorithm.
         try {
-            Assert.assertEquals(sortedArray, unSortedArray, "Array is not Sorted");
-        }catch(Exception ex){
+            Assert.assertEquals(sortedArray, unSortedArray, "selectionSort: Array is not Sorted");
+        } catch (Exception ex) {
             ex.getMessage();
         }
 
         //Now implement Unit test for rest of the soring algorithm...................below
 
+        //Bubble Sort:
+        unSortedArray = new int[]{6, 9, 2, 5, 1, 0, 4};
+        sort.bubbleSort(unSortedArray);
 
+        //Verify if the unsorted array is sorted by the bubble sort algorithm.
+        try {
+            Assert.assertEquals(sortedArray, unSortedArray, "bubbleSort: Array is not Sorted");
+        } catch (Exception ex) {
+            ex.getMessage();
+        }
 
+        //Insertion Sort:
+        unSortedArray = new int[]{6, 9, 2, 5, 1, 0, 4};
+        sort.insertionSort(unSortedArray);
 
+        //Verify if the unsorted array is sorted by the insertion sort algorithm.
+        try {
+            Assert.assertEquals(sortedArray, unSortedArray, "insertionSort: Array is not Sorted");
+        } catch (Exception ex) {
+            ex.getMessage();
+        }
 
+        //Heap Sort:
+        unSortedArray = new int[]{6, 9, 2, 5, 1, 0, 4};
+        sort.heapSort(unSortedArray);
+
+        //Verify if the unsorted array is sorted by the heap sort algorithm.
+        try {
+            Assert.assertEquals(sortedArray, unSortedArray, "heapSort: Array is not Sorted");
+        } catch (Exception ex) {
+            ex.getMessage();
+        }
+
+        //Shell Sort:
+        unSortedArray = new int[]{6, 9, 2, 5, 1, 0, 4};
+        sort.shellSort(unSortedArray);
+
+        //Verify if the unsorted array is sorted by the shell sort algorithm.
+        try {
+            Assert.assertEquals(sortedArray, unSortedArray, "shellSort: Array is not Sorted");
+        } catch (Exception ex) {
+            ex.getMessage();
+        }
+
+        //Merge Sort:
+        unSortedArray = new int[]{6, 9, 2, 5, 1, 0, 4};
+        sort.mergeSort(unSortedArray, 0, unSortedArray.length - 1);
+
+        //Verify if the unsorted array is sorted by the merge sort algorithm.
+        try {
+            Assert.assertEquals(sortedArray, unSortedArray, "mergeSort: Array is not Sorted");
+        } catch (Exception ex) {
+            ex.getMessage();
+        }
+
+        //Quick Sort:
+        unSortedArray = new int[]{6, 9, 2, 5, 1, 0, 4};
+        sort.quickSort(unSortedArray, 0, unSortedArray.length - 1);
+
+        //Verify if the unsorted array is sorted by the quick sort algorithm.
+        try {
+            Assert.assertEquals(sortedArray, unSortedArray, "quickSort: Array is not Sorted");
+        } catch (Exception ex) {
+            ex.getMessage();
+        }
+
+        //Bucket Sort:
+        unSortedArray = new int[]{6, 9, 2, 5, 1, 0, 4};
+        sort.bucketSort(unSortedArray, 9);
+
+        //Verify if the unsorted array is sorted by the bucket sort algorithm.
+        try {
+            Assert.assertEquals(sortedArray, unSortedArray, "bucketSort: Array is not Sorted");
+        } catch (Exception ex) {
+            ex.getMessage();
+        }
     }
 }
+
