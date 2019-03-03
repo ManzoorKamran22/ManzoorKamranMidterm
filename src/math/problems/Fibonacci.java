1d91cb27f3b7a8579ccca738e62e4d1ec684f3fa
Manzoor Kamran <ManzoorKamran@Manzoors-MacBook-Pro.local>
2019-03-03 03:37:56 -0500

    Fibonacci completion

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -1,12 +1,25 @@
 package math.problems;
 
 public class Fibonacci {
+
+    //Fibonacci function
+    public static long fnFibonacci(int n) {
+
+        if (n <= 1) {
+            return n;
+        } else {
+            return fnFibonacci(n - 1) + fnFibonacci(n - 2);
+        }
+    }
+
     public static void main(String[] args) {
          /*
           Write 40 Fibonacci numbers with java.
           0,1,1,2,3,5,8,13
          */
 
-
+        int n = 40;
+        for (int i = 1; i <= n; i++)
+            System.out.println(i + ": " + fnFibonacci(i));
     }
 }
