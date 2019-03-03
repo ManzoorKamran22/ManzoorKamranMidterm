fc095692643f277df6a492cf6de0e32d97c81fdf
Manzoor Kamran <ManzoorKamran@Manzoors-MacBook-Pro.local>
2019-03-03 03:27:48 -0500

    Factorial completion

--- src/math/problems/Factorial.java
+++ src/math/problems/Factorial.java
@@ -1,9 +1,23 @@
 package math.problems;
 
+import java.util.Scanner;
+
 /**
  * Created by mrahman on 04/02/18.
  */
+
 public class Factorial {
+
+    public static int factorial(int n) {
+        int output;
+        if (n == 1) {
+            return 1;
+        }
+
+        //Recursion: Function calling itself
+        output = factorial(n - 1) * n;
+        return output;
+    }
 
     public static void main(String[] args) {
         /*
@@ -12,5 +26,17 @@ public class Factorial {
          *
          */
 
+        //Creating a scanner object to take a user input
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please, enter a number: ");
+
+        //Storing the input value to a variable
+        int num = sc.nextInt();
+
+        //Calling the fact method
+        int factorialResult = factorial(num);
+
+        System.out.println("Factorial of " + num + " is: " + factorialResult);
     }
 }
