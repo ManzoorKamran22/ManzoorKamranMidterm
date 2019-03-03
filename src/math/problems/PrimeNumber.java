50c82ea82a08154e1fcd480f8c0dc1cc60a3c23e
Manzoor Kamran <ManzoorKamran@Manzoors-MacBook-Pro.local>
2019-03-03 04:39:36 -0500

    Prime number code completion

--- src/math/problems/PrimeNumber.java
+++ src/math/problems/PrimeNumber.java
@@ -2,17 +2,45 @@ package math.problems;
 
 public class PrimeNumber {
 
-	public static void main(String[] args) {
-		/*
-		 * Find list of Prime numbers from number 2 to 1 million.
-		 * Try the best solution as possible.Which will take less CPU life cycle.
-		 * Out put number of Prime numbers on the given range.
-		 *
-		 *
-		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
-		 *
-		 */
+    //isPrimeNumber method will return True or False
+    public static boolean isPrimeNumber(int num) {
 
-	}
+        //corner cases: If the number is less than equal to 1 then return False
+        if(num <= 1) {
+            return false;
+        }
 
+        for(int i = 2; i < num; i++) {
+
+            //If the remainder NOT equal to 0 then get out the for loop and return True
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    //method to display how many prime numbers are there based on the given number
+    public static void getPrimeNumbers(int num) {
+
+        for(int i = 2; i <= num; i++) {
+            if(isPrimeNumber(i))
+                System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        /*
+         * Find list of Prime numbers from number 2 to 1 million.
+         * Try the best solution as possible.Which will take less CPU life cycle.
+         * Out put number of Prime numbers on the given range.
+         *
+         *
+         * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
+         *
+         */
+
+        getPrimeNumbers(1000000);
+    }
 }
