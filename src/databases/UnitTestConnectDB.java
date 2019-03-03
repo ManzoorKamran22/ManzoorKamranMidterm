b76c79356329a1244650a2a7442b07ee725f36c8
Manzoor Kamran <ManzoorKamran@Manzoors-MacBook-Pro.local>
2019-03-03 03:04:17 -0500

    DB connection unit testing completion

--- src/databases/UnitTestConnectDB.java
+++ src/databases/UnitTestConnectDB.java
@@ -1,8 +1,12 @@
 package databases;
+import org.testng.Assert;
 
 public class UnitTestConnectDB {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
+
+        Assert.assertEquals(connectToSqlDB.readDataBase("studentInfo","name"),
+                            connectToSqlDB.readDataBase("studentInfo","name"));
 
     }
 }
