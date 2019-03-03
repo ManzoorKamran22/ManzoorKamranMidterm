564cc361c91dd1b7c6534744bdcfb1dbaf074a39
Manzoor Kamran <ManzoorKamran@Manzoors-MacBook-Pro.local>
2019-03-03 01:03:14 -0500

    UseArrayList completion

--- src/datastructure/UseArrayList.java
+++ src/datastructure/UseArrayList.java
@@ -1,4 +1,7 @@
 package datastructure;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
 
 public class UseArrayList {
 
@@ -6,11 +9,34 @@ public class UseArrayList {
 		/*
 		 * Demonstrate how to use ArrayList that includes add,peek,remove,retrieve elements.
 		 * Use For Each loop and while loop with Iterator to retrieve data.
-		 * Store all the sorted data into one of the databases.-no need to do this
+		 * Store all the sorted data into one of the databases.
 		 * 
 		 */
-	
 
+		List<String> arrayList = new ArrayList<String>();
+		arrayList.add("UFT");
+		arrayList.add("Selenium");
+		arrayList.add("DBA");
+		arrayList.add("Mobile Testing");
+		arrayList.add("Manual Testing");
+		arrayList.add("Java Master");
+
+		//For Each Loop - retrieving data from the arrayList
+		System.out.println("Retrieving data from the arrayList by using For Each Loop: ");
+		for (String course : arrayList) {
+			System.out.println(course);
+		}
+
+		//Removing some elements from the arrayList
+		arrayList.remove(5);
+		arrayList.remove("DBA");
+
+		//Iterator
+		Iterator it = arrayList.listIterator();
+
+		System.out.println("\nRetrieving data from the arrayList after removing some elements by using while loop with Iterator: ");
+		while (it.hasNext()) {
+			System.out.println(it.next());
+		}
 	}
-
 }
