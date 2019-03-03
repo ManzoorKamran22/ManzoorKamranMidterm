bdb8133d87c557584b3deb558f2b2eafae27dbfc
Manzoor Kamran <ManzoorKamran@Manzoors-MacBook-Pro.local>
2019-03-03 01:56:38 -0500

    UseMap completion

--- src/datastructure/UseMap.java
+++ src/datastructure/UseMap.java
@@ -1,4 +1,11 @@
 package datastructure;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Iterator;
+
 
 public class UseMap {
 
@@ -11,6 +18,38 @@ public class UseMap {
 		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
 		 */
 
-	}
+		List<String> cityOfUSA = new ArrayList<String>();
+		cityOfUSA.add("New York");
+		cityOfUSA.add("California");
+		cityOfUSA.add("Florida");
 
+		List<String> cityOfUK = new ArrayList<String>();
+		cityOfUK.add("London");
+		cityOfUK.add("Manchester");
+		cityOfUK.add("Birmingham");
+
+		List<String> cityOfCanada = new ArrayList<String>();
+		cityOfCanada.add("Montreal");
+		cityOfCanada.add("Toronto");
+		cityOfCanada.add("Vancouver");
+
+		//key - String type
+		//value - List type which contains String values
+		Map<String, List<String>> map = new LinkedHashMap<String, List<String>>();//LinkedHashMap for sorting order.
+		map.put("USA", cityOfUSA);
+		map.put("UK", cityOfUK);
+		map.put("Canada", cityOfCanada);
+
+		System.out.println("Retrieving data by using For Each Loop: ");
+		for (Entry<String, List<String>> country : map.entrySet()) {
+			System.out.println(country.getKey() + " " + country.getValue());
+		}
+
+		//Iterator
+		Iterator it = map.entrySet().iterator();
+		System.out.println("\nRetrieving data by using while loop with Iterator: ");
+		while (it.hasNext()) {
+			System.out.println(it.next());
+		}
+	}
 }
