5e46d2842f1a4f74d45535a239785827fc4a09f4
Manzoor Kamran <ManzoorKamran@Manzoors-MacBook-Pro.local>
2019-03-04 01:53:35 -0500

    Permutation completion

--- src/string/problems/Permutation.java
+++ src/string/problems/Permutation.java
@@ -1,9 +1,32 @@
 package string.problems;
 
+import java.util.Scanner;
+
 /**
  * Created by mrahman on 04/22/17.
  */
 public class Permutation {
+
+    public void permutation(String str, int startingIndex, int endIndex) {
+        if (startingIndex == endIndex)
+            System.out.println(str);
+        else {
+            for (int i = startingIndex; i <= endIndex; i++) {
+                str = swap(str, startingIndex, i);
+                permutation(str, startingIndex + 1, endIndex);
+                str = swap(str, startingIndex, i);
+            }
+        }
+    }
+
+    public String swap(String strValue, int position1, int position2) {
+        char temp;
+        char[] charArray = strValue.toCharArray();
+        temp = charArray[position1];
+        charArray[position1] = charArray[position2];
+        charArray[position2] = temp;
+        return String.valueOf(charArray);
+    }
 
     public static void main(String[] args) {
 
@@ -12,5 +35,13 @@ public class Permutation {
          * Write Java program to compute all Permutation of a String
          *
          */
+
+        Permutation pr = new Permutation();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please, enter a String to check permutation: ");
+        String inputStr = sc.nextLine();
+
+        pr.permutation(inputStr, 0, inputStr.length() - 1);
+
     }
 }
