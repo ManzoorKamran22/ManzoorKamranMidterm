fdc595b5de5065549d7436d1d2304058eb423d7d
Manzoor Kamran <ManzoorKamran@Manzoors-MacBook-Pro.local>
2019-03-04 00:27:48 -0500

    Duplicate Word completion

--- src/string/problems/DuplicateWord.java
+++ src/string/problems/DuplicateWord.java
@@ -1,9 +1,38 @@
 package string.problems;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by mrahman on 04/22/17.
  */
 public class DuplicateWord {
+
+    public static Set<String> duplicateWords(String s) {
+        if (s == null || s.isEmpty()) {
+            return Collections.emptySet();
+        }
+        Set<String> duplicateWords = new HashSet<>();
+        String[] array = s.split(" ");
+        int avgLength = s.length() / array.length;
+        System.out.println("The average length of the words are: " + avgLength);
+        Set<String> set = new HashSet<>();
+        ArrayList<String> arraylist = new ArrayList<String>();
+        for (String word : array) {
+            arraylist.add(word);
+            if (!set.add(word)) {
+                duplicateWords.add(word);
+            }
+        }
+        for (String word : duplicateWords) {
+            if (Collections.frequency(arraylist, word) > 1) {
+                System.out.println("'" + word +"' occurred " + Collections.frequency(arraylist, word) + " times.");
+            }
+        }
+        return duplicateWords;
+    }
 
     public static void main(String[] args) {
         /*
@@ -12,7 +41,8 @@ public class DuplicateWord {
          */
 
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
-
+        Set<String> duplicates = duplicateWords(st);
+        System.out.println("\nDuplicate words are: " + duplicates);
     }
 
 }
